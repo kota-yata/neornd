a363034bb820fb2be56662ec9a5b5ca04b9073b5
Kota Yatagai <kota@yatagai.com>
2020-08-21 05:04:24 +0000

    Assure specified number of digit after decimal point

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@
 const format = require('biguint-format');
 const crypto = require('crypto');
 
+const COLOR_RESET = '\u001b[0m';
+const COLOR_MAGENTA = '\u001b[35m';
+const COLOR_RED = '\u001b[31m';
+
 /**
  * Get random number with random digits
  * @param {number} quantity - Number of byte to make random decimal
@@ -13,9 +17,37 @@ const GetRandomDecimal = (quantity) => {
   return format(x_byte_buffer, 'dec');
 };
 
-const reset = '\u001b[0m';
-const magenta = '\u001b[35m';
-const red = '\u001b[31m';
+/**
+ * Get random number with specified digits after decimal point.
+ * @param {number} min - Min arg inherited from neornd()
+ * @param {number} max - Max arg inherited from neornd()
+ * @param {number} round - Round arg inherited from neornd()
+ * @return {number} Return random number with specified digits certainly.
+ */
+const GetRandomWithSpecifiedDigits = (min, max, round) => {
+  const MAX_VALUE_OF_4BYTES = 4294967295; // Math.pow(2, 32) - 1 (32bits)
+  const number_for_rounding = Math.pow(10, round);
+  const num_in_range =
+    Math.round(((GetRandomDecimal(4) / MAX_VALUE_OF_4BYTES) * (max - min) + min) * number_for_rounding) /
+    number_for_rounding;
+  if (isCorrectDigits(num_in_range, round)) return num_in_range;
+  return GetRandomWithSpecifiedDigits(min, max, round);
+};
+
+/**
+ * Check if the number of digits after decimal point of 1st arg is same as the number of 2nd arg.
+ * @param {number} rndnum - Random number you generated
+ * @param {number} digits - Number of digits you want
+ * @return {boolean} If the number of digits after decimal point  of 1st arg is the same as the number of 2nd arg, return true.
+ */
+const isCorrectDigits = (rndnum, digits) => {
+  const REGEX_TO_GET_DIGIT = /\.(.+?)$/;
+  const numAfterDecimalPoint = rndnum.toString().match(REGEX_TO_GET_DIGIT);
+  let lengthOfDigits = 0;
+  if (numAfterDecimalPoint !== null) lengthOfDigits = numAfterDecimalPoint[1].length;
+  if (lengthOfDigits === digits) return true;
+  return false;
+};
 
 /**
  * Notify argument error
@@ -23,29 +55,28 @@ const red = '\u001b[31m';
  * @return {NaN} Return NaN
  */
 const ErrorDetect = (str) => {
-  console.error(`${magenta}Warning from neornd : ${red}${str}${reset}`);
+  console.error(`${COLOR_MAGENTA}Error from neornd!! : ${COLOR_RED}${str}${COLOR_RESET}`);
   return NaN;
 };
 
 /**
- * Convert random number generated at GetRandomDecimal() to number in specified range
- * @param {number} min - Minimam number you want
- * @param {number} max - Maximam number you want
- * @param {number} round - The number of digits to appear after the decimal point.
- * @return {number} Return random number with number of digits you want
- **/
-const neornd = (min, max, round) => {
-  if (round < 0) return ErrorDetect('The minimum number of 3rd argument is 0.');
-  if (round > 307) return ErrorDetect('The maximum number of 3rd argument is 307.');
-  if (min > max) return ErrorDetect('1st argument must be smaller than 2nd argument.');
-  const max_value_of_4byte = 4294967295; // Math.pow(2, 32) - 1 (32bits)
-  const number_for_rounding = Math.pow(10, round);
-  const num_in_range =
-    Math.round(((GetRandomDecimal(4) / max_value_of_4byte) * (max - min) + min) * number_for_rounding) /
-    number_for_rounding;
-  return num_in_range;
-};
-
-module.exports = {
-  get: neornd,
+ * @module neornd
+ * @example
+ * const neornd = require('neornd');
+ */
+module.exports = class neornd {
+  /**
+   * Convert random number generated at GetRandomDecimal() to number in specified range
+   * @param {number} min - Minimam number you want
+   * @param {number} max - Maximam number you want
+   * @param {number} round - The number of digits to appear after the decimal point.
+   * @return {number} Return random number with number of digits you want
+   **/
+  static get(min, max, round) {
+    if (round < 0) return ErrorDetect('The minimum number of 3rd argument is 0.');
+    if (round > 16) return ErrorDetect('3rd argument is too big. Maximum call stack size may be exceeded.');
+    if (min > max) return ErrorDetect('1st argument must be smaller than 2nd argument.');
+    const result = GetRandomWithSpecifiedDigits(min, max, round);
+    return result;
+  }
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,57 +2,57 @@ const chai = require('chai');
 const neornd = require('../src/index');
 
 describe('Min Check', () => {
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 100; i++) {
     it('Basic : 1-100-0', () => {
       chai.assert.isAtLeast(neornd.get(1, 100, 0), 1);
     });
     it('1st arg is negative : -4321-5432-10', () => {
       chai.assert.isAtLeast(neornd.get(-4321, 5432, 10), -4321);
     });
-    it('2nd arg is big num : 21-19191919-100', () => {
-      chai.assert.isAtLeast(neornd.get(21, 19191919, 100), 21);
+    it('2nd arg is big num : 21-19191919-4', () => {
+      chai.assert.isAtLeast(neornd.get(21, 19191919, 4), 21);
     });
-    it('3rd arg is maximum : 1-2-307', () => {
-      chai.assert.isAtLeast(neornd.get(1, 2, 307), 1);
+    it('3rd arg is maximum : 1-2-16', () => {
+      chai.assert.isAtLeast(neornd.get(1, 2, 16), 1);
     });
   }
 });
 
 describe('Max Check', () => {
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 100; i++) {
     it('Basic : 1-100-0', () => {
       chai.assert.isAtMost(neornd.get(1, 100, 0), 100);
     });
     it('1st arg is negative : -4321-5432-10', () => {
       chai.assert.isAtMost(neornd.get(-4321, 5432, 10), 5432);
     });
-    it('2nd arg is big num : 21-19191919-100', () => {
-      chai.assert.isAtMost(neornd.get(21, 19191919, 100), 19191919);
+    it('2nd arg is big num : 21-19191919-4', () => {
+      chai.assert.isAtMost(neornd.get(21, 19191919, 4), 19191919);
     });
-    it('3rd arg is maximum : 1-2-307', () => {
-      chai.assert.isAtMost(neornd.get(1, 2, 307), 2);
+    it('3rd arg is maximum : 1-2-16', () => {
+      chai.assert.isAtMost(neornd.get(1, 2, 16), 2);
     });
   }
 });
 
 describe('Error Detection', () => {
-  for (let i = 0; i < 1000; i++) {
-    it('Error! 1st arg is greater than 2nd arg : 1--2-308', () => {
+  for (let i = 0; i < 100; i++) {
+    it('Error! 1st arg is greater than 2nd arg : 1--2-10', () => {
       chai.assert.deepEqual(neornd.get(1, -2, 10), NaN);
     });
-    it('Error! 1st arg is greater than 2nd arg : 1--2-308', () => {
+    it('Error! 1st arg is greater than 2nd arg : 42800-234-10', () => {
       chai.assert.deepEqual(neornd.get(42800, 234, 10), NaN);
     });
-    it('Error! 3rd arg is greater than maximum number : -1-2-308', () => {
-      chai.assert.deepEqual(neornd.get(-1, 2, 308), NaN);
+    it('Error! 3rd arg is greater than maximum number : -1-2-17', () => {
+      chai.assert.deepEqual(neornd.get(-1, 2, 17), NaN);
     });
-    it('Error! 3rd arg is greater than maximum number : 1-2-1000', () => {
+    it('Error! 3rd arg is greater than maximum number : 1-2--1000', () => {
       chai.assert.deepEqual(neornd.get(-11, 2, -1000), NaN);
     });
-    it('Error! 3rd arg is smaller than minimum number : 1-2--1', () => {
+    it('Error! 3rd arg is smaller than minimum number : -11-2--1', () => {
       chai.assert.deepEqual(neornd.get(-11, 2, -1), NaN);
     });
-    it('Error! 3rd arg is smaller than minimum number : 1-2--1000', () => {
+    it('Error! 3rd arg is smaller than minimum number : -11-2--1000', () => {
       chai.assert.deepEqual(neornd.get(-11, 2, -1000), NaN);
     });
   }
