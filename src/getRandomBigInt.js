b28b65cc9d70209e7957c5a09fa062be9806ac98
kota-yata <kota@yatagai.com>
2022-07-14 13:53:37 +0900

    Test passed

--- src/getRandomBigInt.js
+++ src/getRandomBigInt.js
@@ -5,19 +5,17 @@ const MAX_BYTELENGTH = 1073741824; // 2^30
 
 export const getRandomBigInt = (minByteLen, maxByteLen) => {
   const byteLength = getRandomNumber(minByteLen, maxByteLen, 0);
-  console.log('Byte length:', byteLength);
   const quotient = Math.floor(byteLength / MAX_BYTELENGTH);
-  const remainder = byteLength % 30;
+  const remainder = byteLength % MAX_BYTELENGTH;
   let result = 0n;
   for (let i = 0; i < quotient; i++) {
     const buffer = crypto.randomBytes(MAX_BYTELENGTH);
     const bigint = buffer.readBigUInt64LE();
     result = bigint + (result << MAX_BYTELENGTH);
-    console.log(result);
   }
   if (remainder < 1) return result;
   const rBuffer = crypto.randomBytes(remainder);
-  const rBigint = rBuffer.readBigUInt64LE();
-  result = rBigint + (result << BigInt(remainder));
+  const rBigInt = BigInt('0x' + rBuffer.toString('hex'));
+  result = rBigInt + (result << BigInt(remainder * 8));
   return result;
 };
